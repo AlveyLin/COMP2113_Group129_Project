bdd36e69ab061d1bd42fdcb233d0e2b4a7122773
AlveyLin <125428447+AlveyLin@users.noreply.github.com>
2023-05-05 18:25:26 +0800

    Create save_status.h

---
+++ save_status.h
@@ -0,0 +1,9 @@
+// save_status.h
+#ifndef SAVE_STATUS_H
+#define SAVE_STATUS_H
+
+using namespace std;
+
+void save_status(int phealth, int parmor, int pweapon, int pcard[10], int chealth, int carmor, int cweapon, int ccard[10]);
+
+#endif
